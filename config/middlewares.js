3bc9a21cdddd810e04fe408cac98f08096817221
jul <louis.jungo@ti8m.ch>
2022-11-09 13:52:54 +0100

    aws support

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,34 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'shinobi-aws-s3-images-bucket.s3.eu-central-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'shinobi-aws-s3-images-bucket.s3.eu-central-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* End of snippet */
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
